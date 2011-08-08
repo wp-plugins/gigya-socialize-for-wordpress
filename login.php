4c59b1e8f73cd07d3e286a4a0062f0dd60fe5a1f
gilnoy <gilnoy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-08 07:41:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/gigya-socialize-for-wordpress/tags/2.0.5@420507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+	require_once(GIGYA_PLUGIN_PATH.'/resources/login.php');
+	$gigya_login = new GigyaSO_Login();
+	if((float) get_bloginfo('version')<3.1) {
+		$gigya_login->render_js();
+	}
+	
+	$gigya_login->render_css();
+	$gigya_login->render_tmpl();
+?>
+
+
+<script type="text/javascript">
+//<![CDATA[
+jQuery(document).ready(function($) {
+	// Login Dom Manipulation - Add gigya html panel
+	var target = function(){
+		if($("#loginform").length > 0) {
+			return $("#loginform");
+		}
+
+		if($("#registerform").length > 0) { 
+			return $("#registerform");
+		}
+
+		return $("#lostpasswordform");
+	}();
+	
+	(function(elem){
+		elem.wrap("<div class='login-panel login-panel-wp'></div>")
+		.after($("#nav")).parent()
+		.after("<div class='login-panel login-panel-gigya'><form id='componentDiv'></form>")
+		.after("<div class='login-sep-text float-left'><h3>OR</h3></div>");
+
+		<?php if(GIGYA_IS_3_2): ?>
+		$("#nav").after($("#backtoblog"));
+		<?php endif;?>
+		
+	}(target));
+	// Set Ajax Url
+	Gigya.Ajax.setUrl("<?php echo admin_url("admin-ajax.php"); ?>");
+	<?php 
+		$gigya_login->logout();
+		$gigya_login->login(); 
+	?>
+});
+//]]>
+</script>
